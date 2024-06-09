60fdad9d17825b9ed49921d4c1cb7a4382201756
201114771AlterLex <ryuk68.lex@gmail.com>
2024-06-09 00:40:34 -0600

    Test2.1
    Signed-off-by: 201114771AlterLex <ryuk68.lex@gmail.com>

--- calc.js
+++ calc.js
@@ -4,6 +4,7 @@ let x;
 document.getElementById("x").addEventListener("keydown", function(e) {
     if (e.key === "Enter") {
         x = document.getElementById("x").value;
+        console.log(x);
         document.getElementById("log").innerHTML = parse(x);
     }
 });
